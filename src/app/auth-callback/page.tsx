a3a701e0dd00f3e9bd3b91a8d4b81946dda892d6
Michael Bui <michael.bui_crystal@outlook.com>
2024-06-28 17:58:49 +1200

    auth callback and login via kinde

---
+++ src/app/auth-callback/page.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import {useRouter} from "next/navigation";
+
+const Page = () => {
+    const router = useRouter();
+
+    return (
+        <div>
+
+        </div>
+    );
+};
+
+export default Page;
--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,12 +1,16 @@
 import React from 'react';
 import {getKindeServerSession} from "@kinde-oss/kinde-auth-nextjs/server";
+import {redirect} from "next/navigation";
 
-const Page = () => {
-    const {} = getKindeServerSession();
+const Page = async () => {
+    const {getUser} = getKindeServerSession();
+    const user = await getUser();
+
+    if (!user || !user.id) redirect('auth-callback?origin=/dashboard');
 
     return (
         <div>
-            <h1>Dashboard</h1>
+            {user!.email}
         </div>
     );
 };
